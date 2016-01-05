8d550191df0cf49a98fddc8425453c04e7b74497
Leedeoksang <duksang4834@postech.ac.kr>
2016-01-05 13:54:53 +0900

    temp

--- www/js/controllers/intro-login-controller.js
+++ www/js/controllers/intro-login-controller.js
@@ -1,9 +1,7 @@
 angular.module('enertalkHomeUSA.controllers')
 
 	.controller('IntroLoginCtrl', function ($scope, $state) {
-		
 		$scope.login = function () {
-			$state.go('main.myenergy')
+			$state.go('main.myenergy');
 		};
-		
 	});
