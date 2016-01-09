6b3557b7d781174115ffb74a58819e4fb24bd34b
Leedeoksang <duksang4834@postech.ac.kr>
2016-01-09 18:33:30 +0900

    setting image selection

--- www/js/controllers/main-setting-controller.js
+++ www/js/controllers/main-setting-controller.js
@@ -1,5 +1,46 @@
 angular.module('enertalkHomeUSA.controllers')
 
-  .controller('MainSettingCtrl', function($scope) {
+	.controller('MainSettingCtrl', function($scope, User, Util, $cordovaCamera) {
+		function init () {
+			var setting = Util.localStorage.getObject('setting'),
+				settingKeyList = Object.keys(setting);
 
-  });
+			$scope.profile = User.profile;
+			$scope.setting = setting;
+
+			if (settingKeyList.indexOf('enableAutoLogin') < 0) {
+				$scope.setting.enableAutoLogin = true;
+				Util.localStorage.setObject('setting', setting);
+			}
+			if (settingKeyList.indexOf('enableRealtimePopup') < 0) {
+				$scope.setting.enableRealtimePopup = true;
+				Util.localStorage.setObject('setting', setting);
+			}
+
+
+		}
+
+		$scope.changeSetting = function () {
+			var setting = $scope.setting;
+			Util.localStorage.setObject('setting', setting);
+		};
+
+		$scope.changeProfileImage = function () {
+			document.addEventListener("deviceready", function () {
+			    var options = {
+			      destinationType: Camera.DestinationType.FILE_URI,
+			      sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+			    };
+
+			    $cordovaCamera.getPicture(options).then(function(imageURI) {
+			      console.log(imageURI);
+			      // var image = document.getElementById('myImage');
+			      // image.src = imageURI;
+			    }, function(err) {
+			      // error
+			    });
+			});
+		};
+
+		init ();
+	});
--- www/js/services/user.js
+++ www/js/services/user.js
@@ -2,12 +2,14 @@ angular.module('enertalkHomeUSA.services')
 	
 	.service('User', function (Oauth, Api, $log, $q) {
 		
+		var _this = this;
+
 		this.init = function () {
-			this.accesstoken = undefined;
-			this.refreshtoken = undefined;
-			this.uuid = undefined;
-			this.profile = {};
-			this.deviceStatus = 'NOT_REGISTERED';
+			_this.accesstoken = undefined;
+			_this.refreshtoken = undefined;
+			_this.uuid = undefined;
+			_this.profile = {};
+			_this.deviceStatus = 'NOT_REGISTERED';
 		};
 
 		this.login = function (credentials, next) {
@@ -17,9 +19,9 @@ angular.module('enertalkHomeUSA.services')
 				if (response.status === 200 && response.data.access_token) {
 					$log.info('accesstoken is ' + response.data.access_token);
 					$log.info('refreshtoken is ' + response.data.refresh_token);
-					this.accesstoken = response.data.access_token;
-					this.refreshtoken = response.data.refresh_token;
-					return Oauth.getUUID(this.accesstoken);
+					_this.accesstoken = response.data.access_token;
+					_this.refreshtoken = response.data.refresh_token;
+					return Oauth.getUUID(_this.accesstoken);
 				} else {
 					return $q.reject();
 				}
@@ -28,8 +30,8 @@ angular.module('enertalkHomeUSA.services')
 			.then(function (response) {
 				if (response.status === 200 && response.data.uuid) {
 					$log.info('uuid is ' + response.data.uuid);
-					this.uuid = response.data.uuid;
-					return Api.getProfile(this.accesstoken);	
+					_this.uuid = response.data.uuid;
+					return Api.getProfile(_this.accesstoken);	
 				} else {
 					return $q.reject();
 				}
@@ -37,7 +39,7 @@ angular.module('enertalkHomeUSA.services')
 
 			.then(function (response) {
 				if (response.status === 200 && response.data) {
-					this.profile = response.data;
+					_this.profile = response.data;
 				} else {
 					return $q.reject();
 				}
@@ -54,4 +56,5 @@ angular.module('enertalkHomeUSA.services')
 
 		};
 
+		_this.init();
 	});
